38b7095d2895318548bbadba97a71e668b5bbbc5
curreesan <115443503+curreesan@users.noreply.github.com>
2026-01-07 04:35:51 +0530

    Update fixed card size, placeholder for image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,13 @@ import styles from "../styles/Card.module.css";
 export default function Card({ agent, onClick }) {
   return (
     <div className={styles.card} onClick={onClick}>
-      <img src={agent.image} alt={agent.name} className={styles.image} />
+      {agent.image ? (
+        <img src={agent.image} alt={agent.name} className={styles.image} />
+      ) : (
+        <div className={styles.placeholder}>
+          <span className={styles.placeholderName}>{agent.name}</span>
+        </div>
+      )}
       <p className={styles.name}>{agent.name}</p>
     </div>
   );
